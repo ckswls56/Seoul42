2a75bdad3715af40e05a5ddc54c68e68ee340829
Nachanjin <skxkswls@gmail.com>
2022-07-04 01:16:14 +0900

    Add C11

---
+++ C11/ex07/ft_advanced_sort_string_tab.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_advanced_sort_string_tab.c                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cna <cna@stduent.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/08 19:53:56 by cna               #+#    #+#             */
+/*   Updated: 2022/06/09 00:25:15 by cna              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_advanced_sort_string_tab(char **tab, int (*cmp)(char *, char *))
+{
+	int		i;
+	int		j;
+	char	*tmp;
+
+	i = 0;
+	while (tab[i])
+		i++;
+	while (i > 0)
+	{
+		j = 0;
+		while (j < i - 1)
+		{
+			if (cmp(tab[j], tab[j + 1]) > 0)
+			{
+				tmp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = tmp;
+			}
+			j++;
+		}
+		i--;
+	}
+}
